6b777c8b4b06b4e367112cf997c9f8aca103147b
Toan <dinhtoan.vn@gmail.com>
2017-10-30 22:02:12 +0700

    Update config.js

--- api/src/config.js
+++ api/src/config.js
@@ -5,7 +5,7 @@ export const smtp = {
     secure: false, // true for 465, false for other ports
     auth: {
         user: 'apikey', // generated ethereal user
-        pass: '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'  // generated ethereal password
+        pass: 'SendGridApiKey'  // generated ethereal password
     }
 };
 
